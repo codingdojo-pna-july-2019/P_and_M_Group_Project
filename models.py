97c956ced39006d974f70196aef9ac4f08ac16a4
Paul <pphillip@gmail.com>
2019-07-25 20:32:52 -0500

    pray this works

--- models.py
+++ models.py
@@ -6,29 +6,75 @@ from config import db
 #products - id, name, description, unit cost, quantity_available, created_at, updated_at
 #orders_products - id, order_id, product_id
 
+association_table = Table('association', Base.metadata,
+    Column('order_id', Integer, ForeignKey('orders.id')),
+    Column('product_id', Integer, ForeignKey('products.id'))
+)
+
 class Users(db.Model):
   __tablename__ = "users"
   id = db.Column(db.Integer, primary_key=True)
+  f_name = db.Column(db.String(45))
+  l_name = db.Column(db.String(45))
+  email = db.Column(db.String(45))
+  pw = db.Column(db.String(45))
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+class Orders(db.Model):
+  __tablename__ = "orders"
+  products_ordered = relationship (
+   "Products",
+   secondary=Orders_Products,
+   back_populates="orders")
   
-class Dojos(db.Model):
-  __tablename__ = "dojos"
-  #create the table here
+  id = db.Column(db.Integer, primary_key=True)
+  cost = db.Column(db.Integer)
+  paypal_orders_id = db.Column(db.Integer)
+  user_id =  db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+  created_at = db.Column(db.DateTime, server_default=func.now())
+  updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+ 
+class Products(db.Model):
+  __tablename__ = "products"
+  orders_containing_product = relationship (
+   "Orders",
+   secondary=Orders_Products,
+   back_populates="products")
+  
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(45))
-  city = db.Column(db.String(45))
-  state = db.Column(db.String(45))
-  ninjas = db.relationship("Ninjas",backref="dojos", lazy=True)
+  desc = db.Column(db.String(45))
+  unit_cost = db.Column(db.Integer)
   created_at = db.Column(db.DateTime, server_default=func.now())
   updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-  
-class Ninjas(db.Model):
-  __tablename__ = "ninjas"
+
+class Orders_Products(db.Model):
+  __tablename__ = "order_products"
   id = db.Column(db.Integer, primary_key=True)
-  first_name = db.Column(db.String(45))
-  last_name = db.Column(db.String(45))
-  dojo_id = db.Column(db.Integer, db.ForeignKey('dojos.id'), nullable = False)
-  # the below line could be used instead of the relationship in Dojos
-  # dojos = db.relationship('Dojo', foreign_keys=[dojo_id], backref="dojo_ninjas",cascade="all")
   created_at = db.Column(db.DateTime, server_default=func.now())
   updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+
+
+# class Dojos(db.Model):
+#   __tablename__ = "dojos"
+#   create the table here
+#   id = db.Column(db.Integer, primary_key=True)
+#   name = db.Column(db.String(45))
+#   city = db.Column(db.String(45))
+#   state = db.Column(db.String(45))
+#   ninjas = db.relationship("Ninjas",backref="dojos", lazy=True)
+#   created_at = db.Column(db.DateTime, server_default=func.now())
+#   updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+  
+# class Ninjas(db.Model):
+#   __tablename__ = "ninjas"
+#   id = db.Column(db.Integer, primary_key=True)
+#   first_name = db.Column(db.String(45))
+#   last_name = db.Column(db.String(45))
+#   dojo_id = db.Column(db.Integer, db.ForeignKey('dojos.id'), nullable = False)
+#   # the below line could be used instead of the relationship in Dojos
+#   # dojos = db.relationship('Dojo', foreign_keys=[dojo_id], backref="dojo_ninjas",cascade="all")
+#   created_at = db.Column(db.DateTime, server_default=func.now())
+#   updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
