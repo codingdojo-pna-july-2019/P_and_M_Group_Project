7c6111809d729e69c8d15d399e26d5b19eeff752
Melissa <mmankt2@gmail.com>
2019-08-04 20:18:40 -0500

    place order cart fill and pictures

--- models.py
+++ models.py
@@ -5,10 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from config import db
 
 Base = declarative_base()
-#users - id, fn, ln, email, pw, created_at, updated_at
-#orders - id, cost, user_id, created_at,updated_at 
-#products - id, name, description, unit cost, quantity_available, created_at, updated_at
-#orders_products - id, order_id, product_id
 
 orders_products_table = db.Table('orders_products',
     db.Column('order_id', db.Integer, db.ForeignKey('orders.id'), primary_key=True),
@@ -59,33 +55,3 @@ class Shipping(db.Model):
   order = db.relationship("Order",back_populates="shipping")
   created_at = db.Column(DateTime, server_default=func.now())
   updated_at = db.Column(DateTime, server_default=func.now(), onupdate=func.now())
-
-# class Orders_Products(db.Model):
-#   __db.Tablename__ = "order_products"
-#   id = db.db.Column(db.db.Integer, primary_key=True)
-#   created_at = db.db.Column(db.DateTime, server_default=func.now())
-#   updated_at = db.db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-
-
-# class Dojos(db.Model):
-#   __db.Tablename__ = "dojos"
-#   create the db.Table here
-#   id = db.db.Column(db.db.Integer, primary_key=True)
-#   name = db.db.Column(db.String(45))
-#   city = db.db.Column(db.String(45))
-#   state = db.db.Column(db.String(45))
-#   ninjas = db.relationship("Ninjas",backref="dojos", lazy=True)
-#   created_at = db.db.Column(db.DateTime, server_default=func.now())
-#   updated_at = db.db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-  
-# class Ninjas(db.Model):
-#   __db.Tablename__ = "ninjas"
-#   id = db.db.Column(db.db.Integer, primary_key=True)
-#   first_name = db.db.Column(db.String(45))
-#   last_name = db.db.Column(db.String(45))
-#   dojo_id = db.db.Column(db.db.Integer, db.db.ForeignKey('dojos.id'), nullable = False)
-#   # the below line could be used instead of the relationship in Dojos
-#   # dojos = db.relationship('Dojo', foreign_keys=[dojo_id], backref="dojo_ninjas",cascade="all")
-#   created_at = db.db.Column(db.DateTime, server_default=func.now())
-#   updated_at = db.db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-
